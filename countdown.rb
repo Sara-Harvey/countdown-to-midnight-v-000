bab8055776c4dafaef41920d82f3734b94bf9b72
Sara Harvey <saraharveynyc@gmail.com>
2018-09-02 22:15:18 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,17 @@
 def countdown(number)
-  	while number > 0
+  while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-end
-puts "HAPPY NEW YEAR!"
+  end
+  "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  
+ def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end	
 end
